5c67dd3de7932e41cbb259321e6146be71112aff
asus test <msa-99@outlook.com>
2024-09-28 16:41:55 +0600

    useId return a unique id

--- Hooks.js
+++ Hooks.js
@@ -5,4 +5,8 @@ React Hooks and Their Works
  useDebugValue devtoolInspection e array return kore.
  const user={}/[];
  useDebugValue(user, (user)=>user?.bio)
+
+
+ useId: useId sadaronota accesibilt access er jonno id create korar jonno use hoy.
+ form component er label er htmlFor/for and input field er id te useId er id use hoy. input field er attribute "aria-describedBy" er sathe kuno text connection korar jonno text field er id attribute e useId er id use kora hoy. Shadaronoto, input field, label, and eder sathe kuno HTML element sync korar jonno useId er id (zeti unique) bebohar kora hoy.
 */
