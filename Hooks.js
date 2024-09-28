4341dadc320ca0c03c300cca47ea798063290e1e
asus test <msa-99@outlook.com>
2024-09-28 16:18:45 +0600

    useDebugValue hook for devtool inspection

--- Hooks.js
+++ Hooks.js
@@ -1,4 +1,6 @@
 /*
 React Hooks and Their Works
-
+ useDebugValue: custom hook er state debug korar jonno use hoy. condition, handler, useEffect er moddhye useDebugValue use kra zay na. Tobe useDebugValue er moddhye conditionally value deya zay.
+ useDebugValue performance issue kore, tobe er optional function dile shudhu matro devtool open kore useDebugValue exicute hoy.
+ useDebugValue devtoolInspection e array return kore.
 */
