b30fa8d7cd9d92005a6ae9e08e4eb31daad226d5
asus test <msa-99@outlook.com>
2024-09-28 17:23:03 +0600

    useTransion set state update priority

--- Hooks.js
+++ Hooks.js
@@ -9,4 +9,9 @@ React Hooks and Their Works
 
  useId: useId sadaronota accesibilt access er jonno id create korar jonno use hoy.
  form component er label er htmlFor/for and input field er id te useId er id use hoy. input field er attribute "aria-describedBy" er sathe kuno text connection korar jonno text field er id attribute e useId er id use kora hoy. Shadaronoto, input field, label, and eder sathe kuno HTML element sync korar jonno useId er id (zeti unique) bebohar kora hoy.
+
+
+ useTransition: useTransition kuno ekta state update priority differ korte use kora hoy. Ekit component er state update priority beshi thake. Component er kuno ekta state update fast and onnoti slow hole, slow state update er jonno fast er rendering atke thake. Kintu react by default duti state update ke batch korte cay.
+ const [isPending, startTransitio]=useTransition();
+ component er slow update zate fast update ke atke na rakhe (batch update er jonno) se jonno startTransition er callback function er moddhye slow state update code likha hoy. slow stae update shuru-shesh porzonto isPending true and slow state update shesh hole isPending false hoy.
 */
